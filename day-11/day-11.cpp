b3f8dd652738e87ec0f29eb04576fde2d1ac1338
midiphony <midiphony@gmail.com>
2024-12-11 18:43:45 +0100

    Add day-11 part 2 solution
    - Keep counter of stones for each possible stone value
    - Keep results of next potential values (to avoid recalculating them)
    
    Room for improvements :
    - avoid full stoneInfos map copy between each iteration
    - find cycles ?

--- day-11/day-11.cpp
+++ day-11/day-11.cpp
@@ -5,14 +5,65 @@
 
 #include <cmath>
 
+#include <map>
+
 #include <chrono>
+
+struct StoneEntry
+{
+    uint64_t nextPotentialValue;
+    uint64_t nextPotentialValueIfSplit = 0;
+    uint64_t nbOfStones = 0;
+    bool willSplit = false;
+};
+
+std::pair<uint64_t, uint64_t> GetNextPotentialStoneValues(uint64_t initialStoneValue, bool & willSplit)
+{
+    uint64_t nextPotentialValue;
+    uint64_t nextPotentialValueIfSplit = 0;
+    willSplit = false;
+
+    if (initialStoneValue == 0)
+    {
+        nextPotentialValue = 1;
+    }
+    else if (int digitNb = log10(initialStoneValue) + 1; digitNb % 2 == 0)
+    {
+        int middleDigitIndex = digitNb / 2;
+        int powerOfTenSplitValue = pow(10, middleDigitIndex);
+        nextPotentialValue = initialStoneValue / powerOfTenSplitValue;
+        nextPotentialValueIfSplit = initialStoneValue % powerOfTenSplitValue;
+
+        willSplit = true;
+    }
+    else
+    {
+        nextPotentialValue = initialStoneValue * 2024;
+    }
+
+    return {nextPotentialValue, nextPotentialValueIfSplit};
+}
+
+void AddStonesToNextValueInStoneInfos(uint64_t nextValue, uint64_t numberOfStones, std::map<uint64_t, StoneEntry> & newStoneInfos)
+{
+    if (auto search = newStoneInfos.find(nextValue); search != newStoneInfos.end())
+    {
+        search->second.nbOfStones += numberOfStones;
+    }
+    else
+    {
+        bool willSplit = false;
+        auto newNextStoneValues = GetNextPotentialStoneValues(nextValue, willSplit);
+        newStoneInfos[nextValue] = {newNextStoneValues.first, newNextStoneValues.second, numberOfStones, willSplit};
+    }
+}
 
 int main(int argc, char *argv[])
 {
     char *inputFileName;
     if (argc <= 2)
     {
-        std::cout << "Provide input filename AND number of blinks" << std::endl;
+        std::cout << "Provide input filename AND number of blinks (optionally enable exercise 1 via -exo1)" << std::endl;
         return 1;
     }
 
@@ -39,9 +90,11 @@ int main(int argc, char *argv[])
     {
         std::cerr << e.what() << '\n';
     }
-    
 
-    // Ultra brute force method
+    bool enableExo1 = false;
+    if (argc > 3 && std::string(argv[3]) == "-exo1")
+        enableExo1 = true;
+
     std::vector<uint64_t> stones;
     uint64_t stone;
 
@@ -51,26 +104,34 @@ int main(int argc, char *argv[])
         stones.push_back(stone);
     }
 
-    // DEBUG
-    std::cout << "printing stones :\n";
-    for (auto stone : stones)
-    {
-        std::cout << stone << " ";
-    }
-    std::cout << std::endl;
+    // // DEBUG
+    // std::cout << "printing stones :\n";
+    // for (auto stone : stones)
+    // {
+    //     std::cout << stone << " ";
+    // }
+    // std::cout << std::endl;
 
     // Exercise 1 
+    // (Ultra brute force method : not usable for part two)
+    if (enableExo1)
     {
+        std::vector<uint64_t> stonesExo1(stones);
+        int blinksNbExo1 = blinksNb;
+
         auto start = std::chrono::high_resolution_clock::now();
 
-        while (blinksNb > 0)
+        while (blinksNbExo1 > 0)
         {
-            for (int i = 0; i < stones.size(); i++)
+            // DEBUG
+            std::cout << "Blink : " << blinksNbExo1 << std::endl;
+        
+            for (int i = 0; i < stonesExo1.size(); i++)
             {
-                uint64_t stoneValue = stones[i];
+                uint64_t stoneValue = stonesExo1[i];
                 if (stoneValue == 0)
                 {
-                    stones[i] = 1;
+                    stonesExo1[i] = 1;
                     continue;
                 }
                 else if (int digitNb = log10(stoneValue) + 1; digitNb % 2 == 0)
@@ -80,18 +141,18 @@ int main(int argc, char *argv[])
                     int p1 = stoneValue / powerOfTenSplitValue;
                     int p2 = stoneValue % powerOfTenSplitValue;
 
-                    auto posIter = stones.begin() + i;
-                    stones[i] = p2;
-                    stones.insert(posIter, p1);
+                    auto posIter = stonesExo1.begin() + i;
+                    stonesExo1[i] = p2;
+                    stonesExo1.insert(posIter, p1);
                     i++;
                 }
                 else
                 {
-                    stones[i] *= 2024;
+                    stonesExo1[i] *= 2024;
                 }
             }
 
-            blinksNb--;
+            blinksNbExo1--;
         }
 
         auto end = std::chrono::high_resolution_clock::now();
@@ -104,14 +165,60 @@ int main(int argc, char *argv[])
         // }
         // std::cout << "\n\n\n";
 
-        std::cout << "\n\n" << "Exercise 1 result :\n" << stones.size() << "\n\n\n";
+        std::cout << "\n\n" << "Exercise 1 result :\n" << stonesExo1.size() << "\n\n\n";
         std::cout << "\n\n" << "Time to complete :\n" << (duration) << "us"<< "\n\n\n";
     }
 
     // Exercise 2
     {
+        auto start = std::chrono::high_resolution_clock::now();
+
+        // Initialize stone infos
+        std::map<uint64_t, StoneEntry> stoneInfosPerStoneValue;
+        for (auto stoneValue : stones)
+        {
+            bool willSplit = false;
+            std::pair<uint64_t,uint64_t> nextStoneValues = GetNextPotentialStoneValues(stoneValue, willSplit);
+            stoneInfosPerStoneValue[stoneValue] = {nextStoneValues.first, nextStoneValues.second, 1, willSplit};
+        }
+        std::map<uint64_t, StoneEntry> newStoneInfos(stoneInfosPerStoneValue);
+
+        while (blinksNb > 0)
+        {
+            // // DEBUG
+            // std::cout << "Blink : " << blinksNb << std::endl;
+
+            for (auto &stoneInfo : stoneInfosPerStoneValue)
+            {
+                uint64_t currentStoneValue = stoneInfo.first;
+                StoneEntry oldStoneEntry = stoneInfo.second;
+                uint64_t numberOfStones = oldStoneEntry.nbOfStones;
+
+                if (oldStoneEntry.willSplit)
+                    AddStonesToNextValueInStoneInfos(oldStoneEntry.nextPotentialValueIfSplit, numberOfStones, newStoneInfos);
+                
+                AddStonesToNextValueInStoneInfos(oldStoneEntry.nextPotentialValue, numberOfStones, newStoneInfos);
+
+                newStoneInfos[currentStoneValue].nbOfStones -= numberOfStones;
+            }
+
+            stoneInfosPerStoneValue = newStoneInfos;
+
+            blinksNb--;
+        }
+
+        uint64_t finalNbOfStones = 0;
+        for (auto const &stoneInfo : stoneInfosPerStoneValue)
+        {
+            uint64_t previousNbOfStones = finalNbOfStones;
+            finalNbOfStones += stoneInfo.second.nbOfStones;
+        }
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(end - start).count();
         
-        // std::cout << "\n\n" << "Exercise 2 result :\n" << TODO << "\n\n\n";
+        std::cout << "\n\n" << "Exercise 2 result :\n" << finalNbOfStones << "\n\n\n";
+        std::cout << "\n\n" << "Time to complete :\n" << (duration) << "us"<< "\n\n\n";
     }
 
     return 0;
